13815845931df13e8fa0b3d0f6e4b878f086e6e7
0001SID <mailofsumandas@gmail.com>
2018-11-17 12:00:39 +0530

    update

--- text.c
+++ text.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-#include <conio.h>
 int main()
 {
-    FILE *fp;
-    char c;
-    fp = fopen("file.txt","w");
-    fprintf(fp,"This is the content of file");
-    fclose(fp);
-    fopen("file.txt","r");
-    printf("The contents in the file is\n");
-    while((c = fgetc(fp)) != EOF)
-    {
-        printf("%c",c);
-    }
-    fclose(fp);
-    getch();
-    return 0;
+    int a = 7;
+    int remain = 1,sum;
+  sum = ((a + (a - (remain -1)))*((remain-1)/2)) + (a - (remain-1)/2);
+                printf("odd sum = %d",sum);
+   sum = (a + (a - (remain -1)))*(remain/2);
+    printf("even sum = %d ",sum);
+   return 0;
 }
